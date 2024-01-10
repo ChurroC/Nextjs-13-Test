a6c8541a8de377d99f0e0bc774fc3eacbd1d3121
ChurroC <charanvenkatchandran@gmail.com>
2024-01-10 17:55:54 -0600

    accidently commited one folder

---
+++ next_js_full_curse/README.md
@@ -0,0 +1,1 @@
+Thx https://hacksnation.com/d/183-download-all-fireship-courses-free/214. I feel bad for pirating but until I get a job I got to sail the high seas.
--- next_js_full_curse/social_media_modal/src/app/posts/@modal/(.)batman/modal.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useState } from "react";
-
-function Modal({ children }: { children: React.ReactNode }) {
-    const [showModal, setShowModal] = useState(false);
-
-    return <dialog id="modal">{children}</dialog>;
-}
