0f8d29b4321d1346b86b444e2625f88f46490a21
ChurroC <charanvenkatchandran@gmail.com>
2024-04-17 22:55:37 -0500

    moved stuff around for context render

--- context-rerender/src/app/page.tsx
+++ context-rerender/src/app/page.tsx
@@ -1,21 +1,101 @@
 "use client";
 
-import { use, useEffect, useState } from "react";
+import { useNumber, useSetNumber } from "./context";
+import { useNumber2 } from "./context2";
+import crypto from "crypto";
 
 export default function HomePage() {
-    const [theme, setTheme] = useState<string>("system");
+    return (
+        <div className="mx-[10%] flex justify-around">
+            <div className="flex h-screen flex-col items-center justify-around">
+                Using Contexts with Seperate Providers
+                <BothProvider1 />
+                <FirstProvider1 />
+                <SecondProvider1 />
+            </div>
+            <div className="flex h-screen flex-col items-center justify-around">
+                Using Contexts with Single Providers
+                <BothProvider2 />
+                <FirstProvider2 />
+                <SecondProvider2 />
+            </div>
+        </div>
+    );
+}
 
-    useEffect(() => {
-        document.documentElement.setAttribute("class", theme);
-    }, [theme]);
+function Container({ children }: { children: React.ReactNode }) {
+    return (
+        <div
+            key={crypto.randomBytes(4).toString("hex")}
+            className="change-background flex w-fit min-w-52 flex-col gap-3 border p-2"
+        >
+            {children}
+        </div>
+    );
+}
+
+function BothProvider1() {
+    const [number, setNumber] = useNumber();
+    return (
+        <Container>
+            <div>Both Provider Number: {number}</div>
+            <button onClick={() => setNumber(number + 1)}>+ 1</button>
+            <button onClick={() => setNumber(number - 1)}>- 1</button>
+        </Container>
+    );
+}
+
+function FirstProvider1() {
+    const [number] = useNumber();
 
     return (
-        <div className="flex h-screen flex-col items-center justify-center gap-3 dark:text-white">
-            <div>Theme: {theme}</div>
-            <button onClick={() => setTheme("dark")}>Dark</button>
-            <button onClick={() => setTheme("light")}>Light</button>
-            <button onClick={() => setTheme("pink")}>Pink</button>
-            <button onClick={() => setTheme("system")}>System</button>
-        </div>
+        <Container>
+            <div>First Provider Number: {number}</div>
+        </Container>
+    );
+}
+
+function SecondProvider1() {
+    const setNumber = useSetNumber();
+
+    return (
+        <Container>
+            <div>Second Provider Selector:</div>
+            <button onClick={() => setNumber(num => num + 1)}>+ 1</button>
+            <button onClick={() => setNumber(num => num - 1)}>- 1</button>
+        </Container>
+    );
+}
+
+function BothProvider2() {
+    const [number, setNumber] = useNumber2();
+    return (
+        <Container>
+            <div>Both Provider Number: {number}</div>
+            <button onClick={() => setNumber(number + 1)}>+ 1</button>
+            <button onClick={() => setNumber(number - 1)}>- 1</button>
+        </Container>
+    );
+}
+
+function FirstProvider2() {
+    const [number] = useNumber2();
+
+    return (
+        <Container>
+            <div>First Provider Number: {number}</div>
+        </Container>
+    );
+}
+
+function SecondProvider2() {
+    const [, setNumber] = useNumber2();
+
+    return (
+        <Container>
+            <div>Second Provider Selector:</div>
+            <button onClick={() => setNumber(num => num + 1)}>+ 1</button>
+            <button onClick={() => setNumber(num => num - 1)}>- 1</button>
+        </Container>
     );
 }
